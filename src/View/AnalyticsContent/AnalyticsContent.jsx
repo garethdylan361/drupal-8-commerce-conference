79ce6ae5792e12d8daf4386ad5cd96e037413ff9
gareth.small <garethsmall361@gmail.com>
2017-09-07 17:37:33 -0500

    Added chart into panel

--- src/View/AnalyticsContent/AnalyticsContent.jsx
+++ src/View/AnalyticsContent/AnalyticsContent.jsx
@@ -6,10 +6,14 @@ class AnalyticsContent extends React.Component {
 
   render(){
 
-    let Line = window['react-chartjs'].Line;
+    let Line = window['react-chartjs'].Line,
+      Panel = ReactBootstrap.Panel,
+      Col = ReactBootstrap.Col,
+      Row = ReactBootstrap.Row;
 
     const chartData = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["Conference 2011", "Conference 2012", "Conference 2013", "Conference 2014", "Conference 2015",
+        "Conference 2016", "Conference 2017",],
       datasets: [
         {
           label: "My First dataset",
@@ -17,14 +21,20 @@ class AnalyticsContent extends React.Component {
           pointHoverRadius: 5,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40],
+          data: [200, 210, 190, 160, 180, 230, 221],
           spanGaps: false,
         }
       ]
     };
 
     return (
-      <Line data={chartData}/>
+      <Panel header="Conference Registrations">
+        <Row>
+          <Col sm={12}>
+            <Line style={{width: '100%', height:'300px'}} data={chartData}/>
+          </Col>
+        </Row>
+      </Panel>
     );
   }
 }
