d302b36a643987e144ceaaeaa1e9850f93ecde65
gareth.small <garethsmall361@gmail.com>
2017-09-07 09:05:56 -0500

    First commit, began working on skeleton of project.

---
+++ src/Controller/ConferenceDashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\commerce_conference\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+
+/*
+ * ConferenceDashboard Controller
+ * */
+class ConferenceDashboard extends ControllerBase {
+
+  /*
+   * {@inheritdoc}
+   * */
+  public function content(){
+    return [
+      '#theme' => 'commerce_conference_dashboard',
+      '#attached' => array(
+        'library' => array(
+          'commerce_conference/react-js',
+          'commerce_conference/chart-js',
+          'commerce_conference/dashboard-js',
+        ),
+      ),
+    ];
+  }
+}
