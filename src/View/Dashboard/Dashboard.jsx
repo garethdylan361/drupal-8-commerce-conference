480cc3319ef9816ff208664673129e682fab21af
gareth.small <garethsmall361@gmail.com>
2017-09-07 12:20:53 -0500

    Added dashboard component

--- src/View/Dashboard/Dashboard.jsx
+++ src/View/Dashboard/Dashboard.jsx
@@ -1,3 +1,15 @@
-class Dashbard {
+class Dashboard extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <h1>Hello World</h1>
+    );
+  }
 
 }
+
+ReactDOM.render(<Dashboard/>, document.getElementById("conferenceReactApp"));
