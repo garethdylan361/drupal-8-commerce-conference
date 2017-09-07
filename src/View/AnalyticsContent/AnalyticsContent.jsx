8ee118bcb07c81699790290b3d1970210541301c
gareth.small <garethsmall361@gmail.com>
2017-09-07 16:17:03 -0500

    Yahoo :D got ChartJS working with React

--- src/View/AnalyticsContent/AnalyticsContent.jsx
+++ src/View/AnalyticsContent/AnalyticsContent.jsx
@@ -5,8 +5,27 @@ class AnalyticsContent extends React.Component {
   }
 
   render(){
-    return(
-      <h1>Hi :D</h1>
+
+    let Line = window['react-chartjs'].Line;
+
+    const chartData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "My First dataset",
+          fill: false,
+          pointHoverRadius: 5,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40],
+          spanGaps: false,
+        }
+      ]
+    };
+
+    return (
+      <Line data={chartData}/>
     );
   }
 }
+
